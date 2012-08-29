91c6cb2c7d6e074d913f6efc0b8ed3ad68bca21c
Alexander Auritt <alexauritt@gmail.com>
2012-08-29 15:04:16 -0400

    another test

--- spec/country_parser_spec.rb
+++ spec/country_parser_spec.rb
@@ -5,9 +5,11 @@ module WorldBankFetcher
     let(:france) { WorldBank::Country.new('name' => 'France', 'iso2Code' => 'FR')}
     let(:canada) { WorldBank::Country.new('name' => 'Canada', 'iso2Code' => 'CA')}
     let(:arab_world) { WorldBank::Country.new('name' => 'Arab World', 'iso2Code' => '1A')}
-
+    let(:euro_area) {WorldBank::Country.new('name' => "Euro area", 'iso2Code' => "XC")}
+     
     it "should filter expected countries" do
       CountryParser.filter([france, arab_world, canada]).should eq([france, canada])
+      CountryParser.filter([canada, euro_area, france, arab_world]).should eq([canada, france])
     end    
   end  
 end
